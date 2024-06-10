403ed97c4071bcb13079fc159790ea8935762be2
unknown <julian.f.vega@gmail.com>
2024-06-10 17:06:11 -0300

    fix: navbar sections displacement & clipboard pointer

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,11 +1,15 @@
-
+import { Link } from 'react-scroll'
 import styles from './NavBar.module.scss'
-import { Link } from "react-router-dom"
 
 export const NavBar = () => {
     return (
         <div className={styles.navBar}>
-            <span><Link to='#about'>About</Link><Link to='#projects'>Proyectos</Link><Link to='#exp'>Exp</Link><Link to='#contact'>Contacto</Link></span>
+            <span>
+                <Link to='about' smooth={true}>About</Link>
+                <Link to='projects' smooth={true}>Proyectos</Link>
+                <Link to='exp' smooth={true}>Exp</Link>
+                <Link to='contact' smooth={true}>Contacto</Link>
+            </span>
         </div>
     )
 }
